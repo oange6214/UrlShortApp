74d471e678952b0933ac3340bf3af2955c602606
Pohan <oange6214@gmail.com>
2024-05-20 23:40:54 +0800

    feat: Add Action "/shortUrl" generate short url. feat: Add Redirect.

--- src/UrlShortApp/Program.cs
+++ src/UrlShortApp/Program.cs
@@ -19,6 +19,57 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.MapPost("/shortUrl", async (UrlDto url, ApiDbContext db, HttpContext ctx) => 
+{
+    // Validating the input url
+    if (!Uri.TryCreate(url.Url, UriKind.Absolute, out var uri))
+    {
+        return Results.BadRequest("Invalid url has been provided");
+    }
+
+    // Creating a short version of the provided url
+    var random = new Random();
+    const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890@az";
+
+    var randomStr = new string(Enumerable.Repeat(chars, 8)
+                                         .Select(s => s[random.Next(s.Length)])
+                                         .ToArray());
+
+    // Mapping the short url with the long url
+    var sUrl = new UrlManagement()
+    {
+        Url = url.Url,
+        ShortUrl = randomStr
+    };
+
+    // saving the mapping to the db
+    db.Urls.Add(sUrl);
+    await db.SaveChangesAsync();
+
+    // construct url
+    var result = $"{ctx.Request.Scheme}://{ctx.Request.Host}/{sUrl.ShortUrl}";
+
+    return Results.Ok(new UrlShortResponseDto()
+    {
+        Url = result
+    });
+});
+
+app.MapFallback(async (ApiDbContext db, HttpContext ctx) => 
+{
+    var path = ctx.Request.Path.ToUriComponent().Trim('/');
+
+    var urlMatch = await db.Urls.FirstOrDefaultAsync(s => 
+            s.ShortUrl.Trim() == path.Trim());
+
+    if (urlMatch == null)
+    {
+        return Results.BadRequest("Invalid request");
+    }
+
+    return Results.Redirect(urlMatch.Url);
+});
+
 app.Run();
 
 class ApiDbContext : DbContext
